a6e56c6a0d752dbaf40c7bf2c77a91f126478ad9
Johny250353 <72787337+Johny250353@users.noreply.github.com>
2023-04-03 11:37:27 +0200

    Implementacja SQlite
    działa

---
+++ app/build/tmp/kapt3/stubs/debug/com/example/invbike/koszytk_lista_zyczen.java
@@ -0,0 +1,11 @@
+package com.example.invbike;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000\f\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002\u00a8\u0006\u0003"}, d2 = {"Lcom/example/invbike/koszytk_lista_zyczen;", "", "()V", "app_debug"})
+public final class koszytk_lista_zyczen {
+    
+    public koszytk_lista_zyczen() {
+        super();
+    }
+}
