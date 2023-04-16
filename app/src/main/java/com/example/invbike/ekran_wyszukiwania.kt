bd7a276d77368bfbbf51c76b2e404783a5d5f155
Johny250353 <72787337+Johny250353@users.noreply.github.com>
2023-04-16 23:50:03 +0200

    proby dodania firebase

--- app/src/main/java/com/example/invbike/ekran_wyszukiwania.kt
+++ app/src/main/java/com/example/invbike/ekran_wyszukiwania.kt
@@ -1,13 +1,8 @@
 package com.example.invbike
-https:// DATABASE_NAME . InvBike .europe-west1.firebasedatabase.app
-import com.google.firebase.ktx.Firebase
+
 
 class ekran_wyszukiwania {
-    // Write a message to the database
-    val database = Firebase.database
-    val myRef = database.getReference("message")
 
-    myRef.setValue("Hello, World!")
 
 
 }
