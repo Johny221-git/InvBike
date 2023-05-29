1992a87e5475f20e5ef747efbb0c7d621d4377f9
Johny250353 <72787337+Johny250353@users.noreply.github.com>
2023-05-29 12:47:18 +0200

    zmiany build

--- app/src/main/java/com/example/invbikev1/Kalkulator.kt
+++ app/src/main/java/com/example/invbikev1/Kalkulator.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.RadioButton
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 
@@ -21,12 +22,23 @@ class Kalkulator : ComponentActivity() {
         startActivity(PrzejscieOUT)
     }
 
-    fun ButtonLicz(view: View?){
-
+    fun ButtonLicz(view: View?): Int {
+        var mod = 0
         var pisz = findViewById<TextView>(R.id.textViewRozmiar)
         var wartoscA = findViewById<EditText>(R.id.editTextDA)
         var wartoscB = findViewById<EditText>(R.id.editTextDB)
         var wartoscC = findViewById<EditText>(R.id.editTextDC)
+        var radioMen = findViewById<RadioButton>(R.id.radioButton)
+        var radioWom = findViewById<RadioButton>(R.id.radioButton2)
+        var suma = 0
+
+        if(radioMen.isChecked) {
+            mod = 18
+        }
+
+        if(radioWom.isChecked) {
+            mod = 16
+        }
 
         var wA = wartoscA.text.toString()
         var wB = wartoscB.text.toString()
@@ -48,11 +60,22 @@ class Kalkulator : ComponentActivity() {
         }
 
 
-        var suma = intwA + intwB + intwC
+        var sizeA = mod + (intwA - 64)/4 //ogolna
+        var sizeB = intwB/sizeA //nogi
+        var sizeC = intwC/sizeA //tolw
 
-        pisz.setText("${suma}")
 
+        if(sizeB < 0.55 || sizeC < 0.48){
+            suma = sizeA - 2
+            pisz.setText("${suma}")
+        } else (if (sizeB > 0.59 || sizeC > 0.52) {
+                suma = sizeA + 2
+                pisz.setText("${suma}")
+            } else {
+                suma = sizeA
+                pisz.setText("${suma}")
+            })
+
+        return suma
     }
-
-
 }
--- app/src/main/java/com/example/invbikev1/MainActivity.kt
+++ app/src/main/java/com/example/invbikev1/MainActivity.kt
@@ -5,9 +5,13 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var fbAuth: FirebaseAuth;
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,4 +31,24 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+
+    override fun onStart() {
+        super.onStart()
+        isCurrentUser()
+    }
+
+    private fun isCurrentUser() {
+
+        fbAuth.currentUser?.let { auth ->
+            val intent = Intent(applicationContext, MainActivity::class.java).apply {
+                flags = (Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            }
+            startActivity(intent)
+        }
+    }
+
+
 }
+
+
+
